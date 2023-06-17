59b2d9a498d7549245bff39d31954f3d652be983
hubvivek <kumarvivek26121999@gmail.com>
2023-06-17 17:57:32 +0530

    remainder

---
+++ Practice.java
@@ -0,0 +1,14 @@
+public class Practice {
+
+    public static void main(String[] args) {
+        int number = 10;
+        int divisor = 3;
+
+float quotient = number / divisor;
+int remainder = number % divisor;
+
+System.out.println("The quotient is " + quotient);
+System.out.println("The remainder is " + remainder);
+
+    }
+}
